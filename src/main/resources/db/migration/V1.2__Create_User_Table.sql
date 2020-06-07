2c94220fdd8aa2d749a3b82f6f04255a03af5e35
sergio17791 <sergio.m.17791@gmail.com>
2020-06-07 19:07:49 +0200

    Added migrations of user table and postgres uuid-ossp extension

---
+++ src/main/resources/db/migration/V1.2__Create_User_Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "user" (
+    id uuid DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    email VARCHAR(60) NOT NULL UNIQUE,
+    password VARCHAR(512) NOT NULL,
+    salt VARCHAR(60) NOT NULL,
+    active BOOLEAN DEFAULT true,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+    PRIMARY KEY (id)
+);
