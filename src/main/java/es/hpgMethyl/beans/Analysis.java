85c497f453b0485ffb39ad005ddb97478b714e78
sergio17791 <sergio.m.17791@gmail.com>
2020-08-22 15:21:25 +0200

    Added Smith-Waterman algorithm configuration parameters for analysis

--- src/main/java/es/hpgMethyl/beans/Analysis.java
+++ src/main/java/es/hpgMethyl/beans/Analysis.java
@@ -1,5 +1,7 @@
 package es.hpgMethyl.beans;
 
+import java.math.BigDecimal;
+
 import javax.faces.component.UIComponent;
 import javax.servlet.http.Part;
 
@@ -8,12 +10,22 @@ public class Analysis {
 	private String identifier;
 	private String pairedMode;
 	private Part inputReadFile;
+	private BigDecimal swaMinimunScore;
+	private BigDecimal swaMatchScore;
+	private BigDecimal swaMismatchScore;
+	private BigDecimal swaGapOpen;
+	private BigDecimal swaGapExtend;
 	private UIComponent sendAnalysisComponent;
 
 	public Analysis() {
 		this.identifier = "";
 		this.pairedMode = "0";
 		this.inputReadFile = null;
+		this.swaMinimunScore = new BigDecimal("0.8");
+		this.swaMatchScore = new BigDecimal("5.0");
+		this.swaMismatchScore = new BigDecimal("-4.0");
+		this.swaGapOpen = new BigDecimal(10.0);
+		this.swaGapExtend = new BigDecimal(0.5);
 		this.sendAnalysisComponent = null;
 	}
 
@@ -57,6 +69,76 @@ public class Analysis {
 	 */
 	public void setInputReadFile(Part inputReadFile) {
 		this.inputReadFile = inputReadFile;
+	}
+
+	/**
+	 * @return the swaMinimunScore
+	 */
+	public BigDecimal getSwaMinimunScore() {
+		return swaMinimunScore;
+	}
+
+	/**
+	 * @param swaMinimunScore the swaMinimunScore to set
+	 */
+	public void setSwaMinimunScore(BigDecimal swaMinimunScore) {
+		this.swaMinimunScore = swaMinimunScore;
+	}
+
+	/**
+	 * @return the swaMatchScore
+	 */
+	public BigDecimal getSwaMatchScore() {
+		return swaMatchScore;
+	}
+
+	/**
+	 * @param swaMatchScore the swaMatchScore to set
+	 */
+	public void setSwaMatchScore(BigDecimal swaMatchScore) {
+		this.swaMatchScore = swaMatchScore;
+	}
+
+	/**
+	 * @return the swaMismatchScore
+	 */
+	public BigDecimal getSwaMismatchScore() {
+		return swaMismatchScore;
+	}
+
+	/**
+	 * @param swaMismatchScore the swaMismatchScore to set
+	 */
+	public void setSwaMismatchScore(BigDecimal swaMismatchScore) {
+		this.swaMismatchScore = swaMismatchScore;
+	}
+
+	/**
+	 * @return the swaGapOpen
+	 */
+	public BigDecimal getSwaGapOpen() {
+		return swaGapOpen;
+	}
+
+	/**
+	 * @param swaGapOpen the swaGapOpen to set
+	 */
+	public void setSwaGapOpen(BigDecimal swaGapOpen) {
+		this.swaGapOpen = swaGapOpen;
+	}
+
+	/**
+	 * @return the swaGapExtend
+	 */
+	public BigDecimal getSwaGapExtend() {
+		return swaGapExtend;
+	}
+
+	/**
+	 * @param swaGapExtend the swaGapExtend to set
+	 */
+	public void setSwaGapExtend(BigDecimal swaGapExtend) {
+		this.swaGapExtend = swaGapExtend;
 	}
 
 	/**
