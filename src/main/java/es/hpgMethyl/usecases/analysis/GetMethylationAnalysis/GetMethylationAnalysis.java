f528e9b66d6890a815825b3e44a3eebda8455214
Sergio Muñoz <sergio.m.17791@gmail.com>
2021-03-13 07:07:00 -0800

    Added Get Methylation Analysis use case and tests

---
+++ src/main/java/es/hpgMethyl/usecases/analysis/GetMethylationAnalysis/GetMethylationAnalysis.java
@@ -0,0 +1,31 @@
+package es.hpgMethyl.usecases.analysis.GetMethylationAnalysis;
+
+import java.util.UUID;
+
+import es.hpgMethyl.dao.AnalysisRequestDAO;
+import es.hpgMethyl.entities.AnalysisRequest;
+import es.hpgMethyl.exceptions.AnalysisRequestNotFound;
+import es.hpgMethyl.exceptions.HpgMethylException;
+
+public class GetMethylationAnalysis {
+
+	private AnalysisRequestDAO analysisRequestDAO;
+
+	public GetMethylationAnalysis(AnalysisRequestDAO analysisRequestDAO) {
+		this.analysisRequestDAO = analysisRequestDAO;
+	}
+	
+	public GetMethylationAnalysisResponse execute(GetMethylationAnalysisRequest request) throws AnalysisRequestNotFound {
+		
+		UUID id = request.getId();
+		
+		try {
+			AnalysisRequest analysisRequest= this.analysisRequestDAO.get(id.toString());
+			
+			return new GetMethylationAnalysisResponse(analysisRequest);
+			
+		} catch (HpgMethylException exception) {
+			throw new AnalysisRequestNotFound("Analysis Request " + id + " not found", exception);
+		}		
+	}
+}
---
+++ src/test/java/es/hpgMethyl/junit/usecases/analysis/GetMethylationAnalysisTest.java
@@ -0,0 +1,54 @@
+package es.hpgMethyl.junit.usecases.analysis;
+
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import es.hpgMethyl.dao.AnalysisRequestDAO;
+import es.hpgMethyl.entities.AnalysisRequest;
+import es.hpgMethyl.exceptions.AnalysisRequestNotFound;
+import es.hpgMethyl.exceptions.HpgMethylException;
+import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysis;
+import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysisRequest;
+import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysisResponse;
+
+public class GetMethylationAnalysisTest {
+
+	private GetMethylationAnalysis getMethylationAnalysis;
+	
+	private AnalysisRequestDAO analysisRequestDAO;
+	
+	@Before
+	public void setUp() {
+		this.analysisRequestDAO = Mockito.mock(AnalysisRequestDAO.class);
+		this.getMethylationAnalysis = new GetMethylationAnalysis(this.analysisRequestDAO);
+	}
+	
+	@Test(expected = AnalysisRequestNotFound.class)
+	public void test_execute_givenNonExistentMethylationAnalysis_expectThrowAnalysisRequestNotFound() throws HpgMethylException {
+		
+		UUID id = UUID.randomUUID();
+		
+		Mockito.doThrow(HpgMethylException.class).when(analysisRequestDAO).get(id.toString());
+		
+		this.getMethylationAnalysis.execute(new GetMethylationAnalysisRequest(id));
+	}
+	
+	@Test
+	public void test_execute_givenExistentID_expectAnalysisRequest() throws HpgMethylException {
+		
+		UUID id = UUID.randomUUID();
+		
+		AnalysisRequest analysisRequest = new AnalysisRequest();
+		analysisRequest.setId(id);
+		
+		Mockito.when(analysisRequestDAO.get(id.toString())).thenReturn(analysisRequest);
+		
+		GetMethylationAnalysisResponse response = this.getMethylationAnalysis.execute(new GetMethylationAnalysisRequest(id));
+		
+		Assert.assertEquals(id, response.getAnalysisRequest().getId());
+	}
+}
