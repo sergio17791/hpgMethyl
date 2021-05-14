407245c16d9888fe668b62cea0f8d9f2d90e9188
Sergio Muñoz <sergio.m.17791@gmail.com>
2021-05-14 09:55:56 -0700

    New File entity

---
+++ src/main/java/es/hpgMethyl/entities/File.java
@@ -0,0 +1,112 @@
+package es.hpgMethyl.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "file")
+public class File extends BaseEntity {
+
+	@ManyToOne
+	@JoinColumn(name="\"user\"", nullable=false)
+	private User user;
+	
+	@Column(name = "file_name", nullable = false)
+	private String fileName;
+	
+	@Column(name = "size", nullable = false)
+	private Long size;
+	
+	@Column(name = "content_type", nullable = false)
+	private String contentType;
+	
+	@Column(name = "stored", nullable = false)
+	private Boolean stored;
+	
+	public File() {
+		super();
+		this.stored = true;
+	}
+
+	public File(User user, String fileName, Long size, String contentType, Boolean stored) {
+		super();
+		this.user = user;
+		this.fileName = fileName;
+		this.size = size;
+		this.contentType = contentType;
+		this.stored = stored;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public Long getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the contentType
+	 */
+	public String getContentType() {
+		return contentType;
+	}
+
+	/**
+	 * @param contentType the contentType to set
+	 */
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	/**
+	 * @return the stored
+	 */
+	public Boolean getStored() {
+		return stored;
+	}
+
+	/**
+	 * @param stored the stored to set
+	 */
+	public void setStored(Boolean stored) {
+		this.stored = stored;
+	}
+}
