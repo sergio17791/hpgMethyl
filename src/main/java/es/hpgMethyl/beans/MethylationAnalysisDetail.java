a6f0bd1f141ced85d55e55ec7c4a8a89ab320467
Sergio Muñoz <sergio.m.17791@gmail.com>
2021-05-28 04:36:17 -0700

    Revert "Adding section to modify analysis input file"
    This reverts commit 986d1d1e100f046e3fa623a860a6e0c5b1aaf152.

--- src/main/java/es/hpgMethyl/beans/MethylationAnalysisDetail.java
+++ src/main/java/es/hpgMethyl/beans/MethylationAnalysisDetail.java
@@ -1,14 +1,9 @@
   package es.hpgMethyl.beans;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -17,14 +12,10 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.Part;
 
 import es.hpgMethyl.dao.hibernate.AnalysisRequestDAOHibernate;
-import es.hpgMethyl.dao.hibernate.HPGMethylFileDAOHibernate;
 import es.hpgMethyl.entities.AnalysisRequest;
-import es.hpgMethyl.entities.HPGMethylFile;
 import es.hpgMethyl.entities.User;
 import es.hpgMethyl.exceptions.AnalysisRequestNotFound;
 import es.hpgMethyl.exceptions.AnalysisRequestProcessed;
-import es.hpgMethyl.exceptions.CreateFileException;
-import es.hpgMethyl.exceptions.DuplicatedFile;
 import es.hpgMethyl.exceptions.DuplicatedIdentifier;
 import es.hpgMethyl.exceptions.GetObjectException;
 import es.hpgMethyl.exceptions.UpdateMethylationAnalysisException;
@@ -33,19 +24,9 @@ import es.hpgMethyl.types.UserRole;
 import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysis;
 import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysisRequest;
 import es.hpgMethyl.usecases.analysis.GetMethylationAnalysis.GetMethylationAnalysisResponse;
-import es.hpgMethyl.usecases.analysis.UpdateMethylationAnalysisInputFile.UpdateMethylationAnalysisInputFile;
-import es.hpgMethyl.usecases.analysis.UpdateMethylationAnalysisInputFile.UpdateMethylationAnalysisInputFileRequest;
 import es.hpgMethyl.usecases.analysis.UpdateMethylationAnalysisParameters.UpdateMethylationAnalysisParameters;
 import es.hpgMethyl.usecases.analysis.UpdateMethylationAnalysisParameters.UpdateMethylationAnalysisParametersRequest;
-import es.hpgMethyl.usecases.file.CreateFile.CreateFile;
-import es.hpgMethyl.usecases.file.CreateFile.CreateFileRequest;
-import es.hpgMethyl.usecases.file.ExistsFile.ExistsFile;
-import es.hpgMethyl.usecases.file.ExistsFile.ExistsFileRequest;
-import es.hpgMethyl.usecases.file.ListUserFiles.ListUserFiles;
-import es.hpgMethyl.usecases.file.ListUserFiles.ListUserFilesRequest;
-import es.hpgMethyl.usecases.file.ListUserFiles.ListUserFilesResponse;
 import es.hpgMethyl.utils.FacesContextUtils;
-import es.hpgMethyl.utils.FileUtils;
 
 @ManagedBean(name="analysisDetail")
 @RequestScoped
@@ -55,36 +36,13 @@ public class MethylationAnalysisDetail implements Serializable {
 
 	private String id;	
 	private AnalysisRequest analysisRequest;
-	private Part inputReadFile;
 	private Part pairedEndModeFile;
-	private HPGMethylFile selectedInputReadFile;
-	private HPGMethylFile selectedPairedEndModeFile;
-	private Boolean newInputReadFile;
-	private Boolean newPairedReadFile;
 	private UIComponent updateAnalysisParametersComponent;
-	private UIComponent updateInputFileComponent;
-	private List<HPGMethylFile> userFiles;
 		
 	public MethylationAnalysisDetail() {
 		this.id = null;
 		this.analysisRequest = null;
-		this.newInputReadFile = true;
-		this.newPairedReadFile = true;
-		this.userFiles = new ArrayList<HPGMethylFile>();
-	}
-	
-	@PostConstruct
-	public void init() {
-		
-		User user = (User) FacesContextUtils.getParameterFacesContextSession(FacesContextUtils.SESSION_USER);
-		
-		if(user != null) {
-			ListUserFilesResponse response = new ListUserFiles(new HPGMethylFileDAOHibernate()).execute(
-					new ListUserFilesRequest(user, true)
-			);
-			
-			this.userFiles = response.getFiles();
-		}				
+		this.pairedEndModeFile = null;
 	}
 
 	/**
@@ -114,20 +72,6 @@ public class MethylationAnalysisDetail implements Serializable {
 	public void setAnalysisRequest(AnalysisRequest analysisRequest) {
 		this.analysisRequest = analysisRequest;
 	}
-	
-	/**
-	 * @return the inputReadFile
-	 */
-	public Part getInputReadFile() {
-		return inputReadFile;
-	}
-
-	/**
-	 * @param inputReadFile the inputReadFile to set
-	 */
-	public void setInputReadFile(Part inputReadFile) {
-		this.inputReadFile = inputReadFile;
-	}
 
 	/**
 	 * @return the pairedEndModeFile
@@ -141,62 +85,6 @@ public class MethylationAnalysisDetail implements Serializable {
 	 */
 	public void setPairedEndModeFile(Part pairedEndModeFile) {
 		this.pairedEndModeFile = pairedEndModeFile;
-	}
-
-	/**
-	 * @return the selectedInputReadFile
-	 */
-	public HPGMethylFile getSelectedInputReadFile() {
-		return selectedInputReadFile;
-	}
-
-	/**
-	 * @param selectedInputReadFile the selectedInputReadFile to set
-	 */
-	public void setSelectedInputReadFile(HPGMethylFile selectedInputReadFile) {
-		this.selectedInputReadFile = selectedInputReadFile;
-	}
-
-	/**
-	 * @return the selectedPairedEndModeFile
-	 */
-	public HPGMethylFile getSelectedPairedEndModeFile() {
-		return selectedPairedEndModeFile;
-	}
-
-	/**
-	 * @param selectedPairedEndModeFile the selectedPairedEndModeFile to set
-	 */
-	public void setSelectedPairedEndModeFile(HPGMethylFile selectedPairedEndModeFile) {
-		this.selectedPairedEndModeFile = selectedPairedEndModeFile;
-	}
-
-	/**
-	 * @return the newInputReadFile
-	 */
-	public Boolean getNewInputReadFile() {
-		return newInputReadFile;
-	}
-
-	/**
-	 * @param newInputReadFile the newInputReadFile to set
-	 */
-	public void setNewInputReadFile(Boolean newInputReadFile) {
-		this.newInputReadFile = newInputReadFile;
-	}
-
-	/**
-	 * @return the newPairedReadFile
-	 */
-	public Boolean getNewPairedReadFile() {
-		return newPairedReadFile;
-	}
-
-	/**
-	 * @param newPairedReadFile the newPairedReadFile to set
-	 */
-	public void setNewPairedReadFile(Boolean newPairedReadFile) {
-		this.newPairedReadFile = newPairedReadFile;
 	}
 
 	/**
@@ -214,38 +102,10 @@ public class MethylationAnalysisDetail implements Serializable {
 	}
 
 	/**
-	 * @return the updateInputFileComponent
-	 */
-	public UIComponent getUpdateInputFileComponent() {
-		return updateInputFileComponent;
-	}
-
-	/**
-	 * @param updateInputFileComponent the updateInputFileComponent to set
-	 */
-	public void setUpdateInputFileComponent(UIComponent updateInputFileComponent) {
-		this.updateInputFileComponent = updateInputFileComponent;
-	}
-
-	/**
 	 * @return the pairedModes
 	 */
 	public PairedMode[] getPairedModes() {
 		return PairedMode.values();
-	}
-
-	/**
-	 * @return the userFiles
-	 */
-	public List<HPGMethylFile> getUserFiles() {
-		return userFiles;
-	}
-
-	/**
-	 * @param userFiles the userFiles to set
-	 */
-	public void setUserFiles(List<HPGMethylFile> userFiles) {
-		this.userFiles = userFiles;
 	}
 
 	public String loadAnalysisDetail() {
@@ -378,72 +238,5 @@ public class MethylationAnalysisDetail implements Serializable {
 		}
 				
 		return null;
-	}
-	
-	public String updateInputFile() {
-		
-		User user = (User) FacesContextUtils.getParameterFacesContextSession(FacesContextUtils.SESSION_USER);
-		
-		if(user == null) {
-			return "pretty:home";	
-		}
-				
-		AnalysisRequestBean analysisRequestBean = (AnalysisRequestBean) FacesContextUtils.getBean("analysisBean");				
-		
-		try {
-			HPGMethylFile inputHPGMethylFile = null;
-			if(newInputReadFile) {				
-				inputHPGMethylFile = saveFile(user, inputReadFile, inputReadFile.getSubmittedFileName());
-			} else {
-				inputHPGMethylFile = selectedInputReadFile;
-			}
-			
-			new UpdateMethylationAnalysisInputFile(new AnalysisRequestDAOHibernate()).execute(
-					new UpdateMethylationAnalysisInputFileRequest(
-						UUID.fromString(this.id),
-						inputHPGMethylFile
-					)				
-			);
-			
-			analysisRequestBean.setUpdatedAt(new Date());
-			analysisRequestBean.setInputReadFile(inputHPGMethylFile);
-			
-			String successMessage = FacesContextUtils.geti18nMessage("general.updateSuccessfully");
-			FacesContextUtils.setMessageInComponent(this.updateInputFileComponent, FacesMessage.SEVERITY_INFO, successMessage, successMessage);
-			
-		} catch (DuplicatedFile e) {
-			String duplicatedFileMessage = FacesContextUtils.geti18nMessage("error.duplicatedFile");
-			FacesContextUtils.setMessageInComponent(this.updateInputFileComponent, FacesMessage.SEVERITY_ERROR, duplicatedFileMessage, duplicatedFileMessage);
-		} catch (AnalysisRequestNotFound | AnalysisRequestProcessed | CreateFileException| IOException| UpdateMethylationAnalysisException e) {
-			String defaultErrorMessage = FacesContextUtils.geti18nMessage("error.default");
-			FacesContextUtils.setMessageInComponent(this.updateInputFileComponent, FacesMessage.SEVERITY_ERROR, defaultErrorMessage, defaultErrorMessage);
-		} 
-				
-		return null;
-	}
-	
-	private HPGMethylFile saveFile(User user, Part inputFile, String inputFileName) throws CreateFileException, DuplicatedFile, IOException {
-		
-		Boolean duplicatedFile = new ExistsFile(new HPGMethylFileDAOHibernate()).execute(
-				new ExistsFileRequest(user, inputFileName)
-		).getExists();
-		
-		if(duplicatedFile) {
-			throw new DuplicatedFile();
-		}
-		
-		File uploadedFile = FileUtils.saveFileUploadedByUser(user, inputFile, inputFileName);
-
-		HPGMethylFile hpgMethylFile = new CreateFile(new HPGMethylFileDAOHibernate()).execute(
-			new CreateFileRequest(
-				user,
-				uploadedFile.getName(),
-				uploadedFile.getAbsolutePath(),
-				inputFile.getSize(),
-				inputFile.getContentType()
-			)	
-		).getFile();	
-		
-		return hpgMethylFile;
 	}
 }
