d005713f9c1c9d99172a58c5f8a25ba99373f6f8
sergio17791 <sergio.m.17791@gmail.com>
2021-06-17 04:31:35 +0200

    Created migrations for application configuration

---
+++ src/main/resources/db/migration/V1.5__Create_Configuration_Table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "configuration" (
+    id uuid DEFAULT uuid_generate_v4(),
+    users_directory_absolute_path VARCHAR(255) NOT NULL,
+    hpgMethyl_absolute_path VARCHAR(255) NOT NULL,
+    bwt_index_absolute_path VARCHAR(255) NOT NULL,
+    cpu_threads integer NOT NULL,
+    maximum_user_analysis_pending integer,
+    maximum_user_files_stored integer,
+    read_batch_size integer,
+    write_batch_size integer,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),   
+    CONSTRAINT configuration_pkey PRIMARY KEY (id)
+);
